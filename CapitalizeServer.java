ec357f6e67a9745e02c060d9f7d52518a8a40d33
madalinbenchia <33199272+madalinbenchia@users.noreply.github.com>
2018-06-03 17:09:47 +0300

    Upload1

---
+++ CapitalizeServer.java
@@ -0,0 +1,75 @@
+package cs;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+import frames.Login;
+
+public class CapitalizeServer {
+
+    
+    public static void main(String[] args) throws Exception {
+        System.out.println("The capitalization server is running.");
+        int clientNumber = 0;
+        ServerSocket listener = new ServerSocket(9898);
+        try {
+                new Capitalizer(listener.accept(), clientNumber++).run();
+        } finally {
+            listener.close();
+        }
+    }
+
+    private static class Capitalizer extends Thread {
+        private Socket socket;
+        private int clientNumber;
+
+        public Capitalizer(Socket socket, int clientNumber) {
+            this.socket = socket;
+            this.clientNumber = clientNumber;
+            log("New connection with client# " + clientNumber + " at " + socket);
+        }
+        public void run() {
+            try {
+                BufferedReader in = new BufferedReader(
+                        new InputStreamReader(socket.getInputStream()));
+                PrintWriter out = new PrintWriter(socket.getOutputStream());
+                out.flush();
+                while (true) {
+                	System.out.println("Reading line...");
+                	System.out.println(socket.getPort());
+                    String input = in.readLine();
+                    System.out.println(input);
+                    switch(input)
+                    {
+                    	case "login" :	  
+                    		String u = in.readLine();
+                    		String p = in.readLine();
+                    		String message = Login.serverLogin(u, p);
+                    		out.write(message);
+                    }
+                    if (input == null || input.equals(".")) {
+                        break;
+                    }
+                    //out.println(input.toUpperCase());
+                }
+            } catch (IOException e) {
+                log("Error handling client# " + clientNumber + ": " + e);
+            } finally {
+                try {
+                    socket.close();
+                } catch (IOException e) {
+                    log("Couldn't close a socket, what's going on?");
+                }
+                log("Connection with client# " + clientNumber + " closed");
+            }
+        }
+
+        private void log(String message) {
+            System.out.println(message);
+        }
+    }
+}
